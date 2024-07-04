c640405e614a8ba0058ccf5410f88df3b06e73cd
Mahesh <umamahesh690@gmail.com>
2024-07-04 21:33:56 +0530

    first commit

---
+++ src/test/java/ltd/mahesh/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package ltd.mahesh.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
